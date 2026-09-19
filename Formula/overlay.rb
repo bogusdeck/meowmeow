f9091954f40fca4c24335ebce6baaa52c8f5164b
bogusdeck <tanishvashisth@gmail.com>
2026-09-19 23:37:13 +0530

    update formula for v1.0.2

--- Formula/overlay.rb
+++ Formula/overlay.rb
@@ -1,8 +1,8 @@
 class Overlay < Formula
   desc "Translucent macOS HUD overlay for instant coding challenge translation & AI assistance"
   homepage "https://github.com/bogusdeck/meowmeow"
-  url "https://github.com/bogusdeck/meowmeow/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "cf71510b0a815e0ea2c5f984c4f1d1f2aa3653a5d8e7d3ff13ed953e0620708d"
+  url "https://github.com/bogusdeck/meowmeow/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "1546767385e562732a28e9d6a2184a544c2beb822e13223a07eed9f3bdce8cc7"
   license "MIT"
   depends_on :macos
   depends_on "go" => :build
